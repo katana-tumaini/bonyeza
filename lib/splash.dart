6733758281e8f6cb2445f4944a2f6b0a6340e7c1
Katana <katanatumaini1@gmail.com>
2026-09-08 01:49:56 +0300

    added animated text button

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,36 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/step_background.png'),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/step_background.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/emergency-stopp.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 40),
-            const Text(
-              'bonyeza',
-              style: TextStyle(
-                fontFamily: 'PressStart',
-                fontSize: 24,
-                color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/emergency-stopp.png',
+                width: 200,
+                height: 200,
               ),
-            ),
-          ],
+              const SizedBox(height: 40),
+              const Text(
+                'bonyeza',
+                style: TextStyle(
+                  fontFamily: 'PressStart',
+                  fontSize: 24,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 120),
+              AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'start!',
+                    speed: const Duration(milliseconds: 500),
+                    textStyle: const TextStyle(
+                      fontFamily: 'PressStart',
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+                repeatForever: true,
+              ),
+            ],
+          ),
         ),
       ),
     );
